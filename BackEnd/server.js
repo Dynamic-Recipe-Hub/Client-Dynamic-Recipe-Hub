9e39fa971dbaae6bcc0f8d5cdde403c026630b6a
AbdelRahman-Alwabarneh <abdelRahmanalwabarneh@gmail.com>
2024-09-01 21:48:08 +0300

    structure files

---
+++ BackEnd/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("./Config/config"); // استيراد إعدادات الاتصال بقاعدة البيانات
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.json());
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'http://localhost:1000', // استبدل بعنوان النطاق المسموح به
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // السماح بطرق معينة
+  allowedHeaders: ['Content-Type', 'Authorization'], // السماح برؤوس معينة
+  credentials: true,
+}));
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+// بدء تشغيل الخادم
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
