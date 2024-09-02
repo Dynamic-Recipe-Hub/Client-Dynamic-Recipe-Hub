24323030fd713720654b27ebdedd4b957c5ff7f3
hashem-frehat <hashem.abofreh@gmail.com>
2024-09-02 15:52:57 +0300

    first push

--- FrontEnd/LuxeSweets/src/App.jsx
+++ FrontEnd/LuxeSweets/src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
-
+import "./App.css";
+import Catalogrecipes from "./Pages/catalogrecipes";
 function App() {
-
   return (
     <>
-
+      <Catalogrecipes />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
